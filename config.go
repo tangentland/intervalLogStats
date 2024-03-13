940261514c8922a908ff6a220bf66f9f018762a5
Todd Holley <todd@tangentland.net>
2024-03-13 14:05:31 -0700

    commect out filterottl usage

--- config.go
+++ config.go
@@ -3,9 +3,7 @@ package intervalLogStats
 import (
 	"fmt"
 
-	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/confmap"
-	"go.uber.org/zap"
 	//"github.com/open-telemetry/opentelemetry-collector-contrib/internal/filter/filterottl"
 	//"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl"
 )
@@ -45,9 +43,9 @@ func (c *Config) Validate() error {
 		if name == "" {
 			return fmt.Errorf("metrics: metric name missing")
 		}
-		if _, err := filterottl.NewBoolExprForMetric(info.Conditions, filterottl.StandardMetricFuncs(), ottl.PropagateError, component.TelemetrySettings{Logger: zap.NewNop()}); err != nil {
-			return fmt.Errorf("metrics condition: metric %q: %w", name, err)
-		}
+		//if _, err := filterottl.NewBoolExprForMetric(info.Conditions, filterottl.StandardMetricFuncs(), ottl.PropagateError, component.TelemetrySettings{Logger: zap.NewNop()}); err != nil {
+		//	return fmt.Errorf("metrics condition: metric %q: %w", name, err)
+		//}
 		if len(info.Attributes) > 0 {
 			return fmt.Errorf("metrics attributes not supported: metric %q", name)
 		}
@@ -57,9 +55,9 @@ func (c *Config) Validate() error {
 		if name == "" {
 			return fmt.Errorf("datapoints: metric name missing")
 		}
-		if _, err := filterottl.NewBoolExprForDataPoint(info.Conditions, filterottl.StandardDataPointFuncs(), ottl.PropagateError, component.TelemetrySettings{Logger: zap.NewNop()}); err != nil {
-			return fmt.Errorf("datapoints condition: metric %q: %w", name, err)
-		}
+		//if _, err := filterottl.NewBoolExprForDataPoint(info.Conditions, filterottl.StandardDataPointFuncs(), ottl.PropagateError, component.TelemetrySettings{Logger: zap.NewNop()}); err != nil {
+		//	return fmt.Errorf("datapoints condition: metric %q: %w", name, err)
+		//}
 		if err := info.validateAttributes(); err != nil {
 			return fmt.Errorf("spans attributes: metric %q: %w", name, err)
 		}
@@ -68,9 +66,9 @@ func (c *Config) Validate() error {
 		if name == "" {
 			return fmt.Errorf("logs: metric name missing")
 		}
-		if _, err := filterottl.NewBoolExprForLog(info.Conditions, filterottl.StandardLogFuncs(), ottl.PropagateError, component.TelemetrySettings{Logger: zap.NewNop()}); err != nil {
-			return fmt.Errorf("logs condition: metric %q: %w", name, err)
-		}
+		//if _, err := filterottl.NewBoolExprForLog(info.Conditions, filterottl.StandardLogFuncs(), ottl.PropagateError, component.TelemetrySettings{Logger: zap.NewNop()}); err != nil {
+		//	return fmt.Errorf("logs condition: metric %q: %w", name, err)
+		//}
 		if err := info.validateAttributes(); err != nil {
 			return fmt.Errorf("logs attributes: metric %q: %w", name, err)
 		}
